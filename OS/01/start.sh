5f22885e753291889df334a416e0e28f779af46b
killian <63927363+KillianLucas@users.noreply.github.com>
2024-01-27 21:28:06 -0800

    Fixes

--- OS/01/start.sh
+++ OS/01/start.sh
@@ -1,13 +1,13 @@
 ### Start whisper.cpp and stuff?
 
+### APP
+
+open app/index.html
+# ^ This should be to run it in fullscreen / kiosk mode
+
 ### CORE
 
 cd core/
 pip install poetry
 poetry install
 poetry run bash start.sh
-
-### APP
-
-open app/index.html
-# ^ This should be to run it in fullscreen / kiosk mode
