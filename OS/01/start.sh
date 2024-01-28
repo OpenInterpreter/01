3beba042d42bc4744bd207e8f23364e32aad6e3b
killian <63927363+KillianLucas@users.noreply.github.com>
2024-01-27 20:24:24 -0800

    Added init and added chromium to start script

--- OS/01/start.sh
+++ OS/01/start.sh
@@ -1,12 +1,19 @@
+### Install Chromium if not already installed
+if ! command -v chromium-browser &> /dev/null
+then
+    apt-get install chromium-browser
+fi
+
 ### APP
 
-# Display app/index.html on the second monitor in full-screen mode
-google-chrome --kiosk --app=file:///app/index.html
+# Dynamically get path to chrome executable and use it here
+CHROME_PATH=$(which chromium-browser)
+$CHROME_PATH --kiosk ----app=file:///app/index.html
 
 ### Start whisper.cpp and stuff?
 
 ### CORE
 
-cd /core
+cd core/
 poetry install
 poetry run bash start.sh
