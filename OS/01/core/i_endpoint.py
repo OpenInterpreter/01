3e10fb80fa63f9d2def6dbe2eb8c221019ccfd28
Zohaib Rauf <zabirauf@gmail.com>
2024-02-02 21:16:34 -0800

    Added service for transcription

--- OS/01/core/i_endpoint.py
+++ OS/01/core/i_endpoint.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, Request, WebSocket
 import uvicorn
 import redis
+import json
+from dotenv import load_dotenv
+from stt import get_transcription
+import tempfile
+
+# Load environment variables
+load_dotenv()
 
 app = FastAPI()
 
@@ -10,7 +17,7 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 @app.post("/i/")
 async def i(request: Request):
     message = await request.json()
-    
+
     client_host = request.client.host  # Get the client's IP address
 
     message = f"""
@@ -26,5 +33,47 @@ async def i(request: Request):
         "content": message
     })
 
+
+@app.websocket("/a")
+async def a(ws: WebSocket):
+    await ws.accept()
+    audio_file = bytearray()
+    mime_type = None
+
+    try:
+        while True:
+            message = await ws.receive()
+
+            if message['type'] == 'websocket.disconnect':
+                break
+
+            if message['type'] == 'websocket.receive':
+                if 'text' in message:
+                    control_message = json.loads(message['text'])
+                    if control_message.get('action') == 'command' and control_message.get('state') == 'start' and 'mimeType' in control_message:
+                        # This indicates the start of a new audio file
+                        mime_type = control_message.get('mimeType')
+                    elif control_message.get('action') == 'command' and control_message.get('state') == 'end':
+                        # This indicates the end of the audio file
+                        # Process the complete audio file here
+                        transcription = get_transcription(audio_file, mime_type)
+                        await ws.send_json({"transcript": transcription})
+                        
+                        print("SENT TRANSCRIPTION!")
+
+                        # Reset the bytearray for the next audio file
+                        audio_file = bytearray()
+                        mime_type = None
+                elif 'bytes' in message:
+                    # If it's not a control message, it's part of the audio file
+                    audio_file.extend(message['bytes'])
+    except Exception as e:
+        print(f"WebSocket connection closed with exception: {e}")
+    finally:
+        await ws.close()
+        print("WebSocket connection closed")
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    with tempfile.TemporaryDirectory():
+        uvicorn.run(app, host="0.0.0.0", port=8000)
