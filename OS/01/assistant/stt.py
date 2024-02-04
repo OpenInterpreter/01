13f5695c4f65321e4e22e4ddcf629ec1338de6c8
killian <63927363+KillianLucas@users.noreply.github.com>
2024-02-03 18:09:52 -0800

    Updates

---
+++ OS/01/assistant/stt.py
@@ -0,0 +1,6 @@
+"""
+Defines a function which takes a path to an audio file and turns it into text.
+"""
+
+def stt(path_to_audio):
+    return text
---
+++ OS/01/assistant/tts.py
@@ -0,0 +1,6 @@
+"""
+Defines a function which takes text and returns a path to an audio file.
+"""
+
+def tts(text):
+    return path_to_audio
