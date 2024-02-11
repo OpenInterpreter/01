ae39b12a70fb67d7cd753567b8c644a8ac2f3731
birbbit <hristijan.bogoevski@gmail.com>
2024-02-11 02:53:23 -0800

    Skills added dynamically to prompt

--- OS/01/skills/schedule.py
+++ OS/01/skills/schedule.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import json
 import subprocess
 
-def add_message_to_queue(message):
 
+def _add_message_to_queue(message):
     # Define the message data and convert it to JSON
     message_json = json.dumps({
         "role": "computer",
@@ -14,12 +14,14 @@ def add_message_to_queue(message):
     })
     subprocess.run(['logger', '{TO_INTERPRETER{' + message_json + '}TO_INTERPRETER}'])
 
-def schedule(dt, message):
+
+def schedule(dt: datetime, message: str) -> None:
+    """"Schedules a reminder at a specific time. At the specified time, the message will be added to the queue."""
     # Calculate the delay in seconds
     delay = (dt - datetime.now()).total_seconds()
 
     # Create a timer
-    timer = threading.Timer(delay, add_message_to_queue, args=[message])
+    timer = threading.Timer(delay, _add_message_to_queue, args=[message])
 
     # Start the timer
     timer.start()
