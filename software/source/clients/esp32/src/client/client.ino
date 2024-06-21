2814e1ff5dbb82f7fc92183b119d2f66f2fcf01b
Ben Xu <benx.xu@mail.utoronto.ca>
2024-06-20 23:56:03 -0700

    add mic buf count and len settings

--- software/source/clients/esp32/src/client/client.ino
+++ software/source/clients/esp32/src/client/client.ino
@@ -638,6 +638,8 @@ void InitI2SSpeakerOrMic(int mode)
         i2s_config.mode =
             (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM);
         i2s_config.sample_rate = MIC_SAMPLE_RATE;
+        i2s_config.dma_buf_count = 8;
+        i2s_config.dma_buf_len = 240;
     }
     else
     {
